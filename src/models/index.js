2db4fcd5286d126db3628bfdc44d7f35e7488dde
Hugo Esteban Quinteros Bustos <76698088+HQB92@users.noreply.github.com>
2024-07-22 10:53:35 -0400

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -2,10 +2,12 @@ const Church = require('./church');
 const User = require('./user');
 const Member = require('./member');
 const Status = require('./status');
+const BaptismRecord = require('./baptismRecord');
 
 module.exports = {
     Church,
     User,
     Member,
-    Status
+    Status,
+    BaptismRecord
 }
