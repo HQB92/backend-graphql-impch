c9080bd03097f25132e649b2701d927b392b1b86
Hugo Esteban Quinteros Bustos <hquinteros@ing.ucsc.cl>
2024-07-23 13:48:55 -0400

    fix index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,21 @@ const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const cors = require('cors');
 
-
 require('dotenv').config();
 
 const app = express();
 
-// Habilitar CORS para todas las solicitudes
-app.use(cors());
+// Habilitar CORS para dominios específicos
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:4000', 'https://impchzanartu.online' ];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 app.use(express.json());
 app.use('/auth', authRouter);
@@ -21,12 +29,16 @@ const authMiddleware = ({ req }) => {
   const authHeader = req.headers.authorization;
   if (authHeader) {
     const token = authHeader.split(' ')[1];
-    const decoded = verifyToken(token);
-    const currentTime = Math.floor(Date.now() / 1000); // Tiempo actual en segundos
-    if (decoded.exp && decoded.exp < currentTime) {
-      throw new Error('Token expirado');
+    try {
+      const decoded = verifyToken(token);
+      const currentTime = Math.floor(Date.now() / 1000); // Tiempo actual en segundos
+      if (decoded.exp && decoded.exp < currentTime) {
+        throw new Error('Token expirado');
+      }
+      return { user: decoded };
+    } catch (err) {
+      throw new Error('Invalid/Expired token');
     }
-    return { user: decoded };
   }
   throw new Error('Authorization header must be provided');
 };
@@ -42,9 +54,9 @@ server.start().then(() => {
   server.applyMiddleware({ app });
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => {
-    console.log(
-      `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
-    );
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
     console.log(`🚀 Login endpoint at http://localhost:${PORT}/auth/login`);
   });
+}).catch((error) => {
+  console.error('Error starting the server: ', error);
 });
