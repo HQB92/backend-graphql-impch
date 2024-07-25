b97dfc51e426d08142a87f9e767ed6ac9eae9e89
Hugo Esteban Quinteros Bustos <quinteros.hugo@externos.bci.cl>
2024-07-25 11:43:14 -0400

    Update users.js

--- src/services/users.js
+++ src/services/users.js
@@ -1,22 +1,31 @@
 const bcrypt = require('bcryptjs');
 const User = require('../db/models/user');
 const Member = require('../db/models/member');
+const sequelize = require('../config/database');
 
 // Crear un usuario (usado para pruebas)
 const createUser = async (args) => {
     args.password = bcrypt.hashSync("123456", 10);
-    const member = await Member.findOne({ where: { rut: args.rut } });
-    if (!member) {
-        return { code: 403, message: 'Miembro no existe' };
+    const transaction = await sequelize.transaction();
+
+    try {
+        const member = await Member.findOne({ where: { rut: args.rut } }, transaction);
+        if (!member) {
+            return { code: 403, message: 'Miembro no existe' };
+        }
+
+        delete args.id;
+        const user = await User.create(args,transaction);
+        const meberUpdate = await Member.update({ userId: user.dataValues.id }, { where: { rut: args.rut } }, transaction);
+        if ( user && meberUpdate[0] === 1) {
+            return {code: 200, message: 'Usuario creado Exitosamente'};
+        } else {
+            return {code: 400, message: 'Error al crear usuario'};
+        }
+    } catch (error) {
+        await transaction.rollback();
+        return {code: 500, message: 'Error al crear usuario'};
     }
-    delete args.id;
-    const user = await User.create(args);
-    const meberUpdate = await Member.update({ userId: user.dataValues.id }, { where: { rut: args.rut } });
-    if ( user && meberUpdate[0] === 1) {
-        return {code: 200, message: 'Usuario creado Exitosamente'};
-    } else {
-        return {code: 400, message: 'Error al crear usuario'};
-    }    
 };
 
 // Buscar un usuario por nombre de usuario
@@ -33,38 +42,51 @@ const findAllUsers = async () => {
 }
 
 const updateUser = async (args) => {
-    const {user} = args
-    if (user.password) {
-        user.password = bcrypt.hashSync(user.password, 10);
-        try {
-            const userData = await User.update(
-                { password:user.password }, {where: {id: user.id}});
-            console.log(userData);
-            return {code: 200, message: 'Usuario Actualizado Exitosamente'};
-        }catch (error) {
-            console.log(error);
-            return {code: 400, message: 'Error al actualizar usuario'};
-        }
-    }
-    try {
-        const userData = await User.update(
-            { roles:user.roles, email:user.email }, {where: {id: user.id}});
-        console.log(userData);
-        return {code: 200, message: 'Usuario Actualizado Exitosamente'};
-    } catch (error) {
-        console.log(error);
-        return {code: 400, message: 'Error al actualizar usuario'};
-    }
+    const { user } = args;
 
-}
+    const transaction = await sequelize.transaction(); // Inicia una nueva transacción
+
+    try {
+        if (user.password) {
+            user.password = bcrypt.hashSync(user.password, 10);
+            // Actualiza el usuario con la nueva contraseña
+            await User.update(
+                { password: user.password },
+                { where: { id: user.id }, transaction }
+            );
+        }
+
+        // Actualiza el usuario con roles y email
+        await User.update(
+            { roles: user.roles, email: user.email },
+            { where: { id: user.id }, transaction }
+        );
+
+
+
+        return { code: 200, message: 'Usuario Actualizado Exitosamente' };
+    } catch (error) {
+        // Revierte la transacción en caso de error
+        await transaction.rollback();
+        return { code: 400, message: 'Error al actualizar usuario' };
+    }
+};
 
 const deleteUser = async (id) => {
-    const data = await User.destroy({ where: { id } });
-    if (data === 1) {
-        return { code: 200 , message: 'Usuario Eliminado Exitosamente' };
-    }else {
-        return {code: 400, message: 'Error al eliminar usuario'};
+    const transaction = await sequelize.transaction();
+    try {
+        const data = await User.destroy({ where: { id } }, transaction );
+        await transaction.commit();
+        if (data === 1) {
+            return { code: 200 , message: 'Usuario Eliminado Exitosamente' };
+        }else {
+            return {code: 400, message: 'Error Usuario no existe'};
+        }
+    } catch (e) {
+        await transaction.rollback();
+        return {code: 500, message: 'Error al eliminar usuario'};
     }
+
 }
 
 const changePassword = async (id, password) => {
