1fae80c41b42aaf7662e09d27353893715e5a1ca
Hugo Esteban Quinteros Bustos <quinteros.hugo@externos.bci.cl>
2024-07-26 13:25:45 -0400

    fix init resolver

--- src/graphql/resolvers/offering.resolver.js
+++ src/graphql/resolvers/offering.resolver.js
@@ -1,5 +1,13 @@
 
 const {validateContext} = require('../../utils/tokensLogs');
 const logger = require('../../utils/logger');
-const resolversOffering = {}
+const resolversOffering = {
+  query: {
+    hallo: async (args, context) => {
+      return {
+        message: 'Hallo, World!'
+      }
+    }
+  }
+}
 module.exports = resolversOffering;
--- src/graphql/typeDefs/offering.typeDef.js
+++ src/graphql/typeDefs/offering.typeDef.js
@@ -2,9 +2,7 @@ const { gql } = require('apollo-server-express');
 
 const dataTypesOffering = gql`
   type holle{
-    id: ID!
-    name: String!
-    address: String
+    message: String
   }
   
 
