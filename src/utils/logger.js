9453a1317f4c5e154fbd68e8e6ff6fa3ba257ccb
Hugo Esteban Quinteros Bustos <quinteros.hugo@externos.bci.cl>
2024-07-27 12:29:11 -0400

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -25,6 +25,7 @@ const logger = {
         console.error(`${operation} - Error:`, error);
     },
     logUser: (operation, user) => {
+			console.log("user",user)
         console.log(`${operation} - User:`, user.username);
     },
     logAuthUsername: (operation, username) => {
