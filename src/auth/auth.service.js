7f69daf7d575f553f6029a682e7d17a926f166a3
Hugo Esteban Quinteros Bustos <76698088+HQB92@users.noreply.github.com>
2024-07-28 22:26:02 -0400

    Update auth.service.js

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -1,21 +1,20 @@
-const bcrypt = require('bcryptjs');
-const { generateToken } = require('../utils/auth');
-const { findUserByUsername } = require('../services/users.service');
-const { userLogs, passwordLogs } = require('../utils/tokensLogs');
+const {generateToken} = require('../utils/auth');
+const {userLogs, passwordLogs} = require('../utils/tokensLogs');
 const logger = require('../utils/logger');
 
 const login = async (username, password) => {
-    const operation = 'Auth - Login';
-    logger.logStart(operation);
-    logger.logAuthUsername(operation, username);
-    logger.logAuthPassword(operation, password);
-    const user = await findUserByUsername(username);
-    userLogs(user);
-    passwordLogs(password, user);
-    const token = generateToken(user.id, username, user.email, user.rut, user.roles);
-    logger.logToken(operation, token);
-    logger.logEnd(operation);
-    return token;
+	const {findUserByUsername} = require('../services/users.service');
+	const operation = 'Auth - Login';
+	logger.logStart(operation);
+	logger.logAuthUsername(operation, username);
+	logger.logAuthPassword(operation, password);
+	const user = await findUserByUsername(username);
+	userLogs(user);
+	passwordLogs(password, user);
+	const token = generateToken(user.id, username, user.email, user.rut, user.roles);
+	logger.logToken(operation, token);
+	logger.logEnd(operation);
+	return token;
 };
 
-module.exports = { login };
+module.exports = {login};
