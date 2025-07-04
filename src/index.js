48abdccb6678cd375c522efc87cf035abbf5bb5c
Hugo Esteban Quinteros Bustos <technologiesgq@gmail.com>
2025-07-04 00:01:37 -0400

    Eliminar configuración de credenciales y métodos específicos en CORS, simplificando la gestión de solicitudes.

--- src/index.js
+++ src/index.js
@@ -18,9 +18,6 @@ app.use('/auth', authRouter);
 
 app.use(cors({
   origin: '*',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 const authMiddleware = ({ req }) => {
