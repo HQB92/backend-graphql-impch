3ab8ff4705c351b21b5a2a7c774c59b746be1ce4
Hugo Esteban Quinteros Bustos <technologiesgq@gmail.com>
2025-07-03 23:59:59 -0400

    Agregar configuración de CORS para permitir solicitudes desde cualquier origen, incluyendo métodos y encabezados específicos.

--- src/index.js
+++ src/index.js
@@ -16,6 +16,13 @@ const app = express();
 app.use(express.json());
 app.use('/auth', authRouter);
 
+app.use(cors({
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 const authMiddleware = ({ req }) => {
   const authHeader = req.headers.authorization;
   //obtener ip
