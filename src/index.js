6b938a76fd493e522ea0d6a3ca9344181318a3df
Hugo Esteban Quinteros Bustos <technologiesgq@gmail.com>
2025-07-03 23:48:53 -0400

    Eliminar la configuración de CORS para dominios específicos, simplificando la gestión de solicitudes. Se mantiene el uso de express.json() y la ruta de autenticación.

--- src/index.js
+++ src/index.js
@@ -13,31 +13,6 @@ require('dotenv').config();
 
 const app = express();
 
-// Habilitar CORS para dominios específicos
-const allowedOrigins = [
-  'http://localhost:3000',
-  'http://localhost:4000',
-  'https://localhost:3000',
-  'https://localhost:4000',
-  'https://impchzanartu.online',
-  'https://app.impchzanartu.online',
-  'https://studio.apollographql.com',
-  'http://impchzanartu.cl',
-  'http://api.impchzanartu.cl',
-  'https://impchzanartu.cl'
-];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      logger.logErrorCors("CORS Error", origin);
-      callback(new Error('No permitido por CORS'));
-    }
-  },
-  credentials: true
-}));
-
 app.use(express.json());
 app.use('/auth', authRouter);
 
