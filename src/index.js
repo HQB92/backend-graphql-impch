01bb8a90d8a4b6216d3f5ac503354ff29e6eb962
Hugo Esteban Quinteros Bustos <technologiesgq@gmail.com>
2025-07-04 01:00:22 -0400

    Eliminar la opción de credenciales en la configuración de CORS, simplificando la gestión de solicitudes permitidas.

--- src/index.js
+++ src/index.js
@@ -27,7 +27,6 @@ app.use(cors({
       callback(new Error('No permitido por CORS'));
     }
   },
-  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
